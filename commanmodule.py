8a50289527941ab2e1d1e38a0ea4e590ce46df55
jainaaditya <adityajainmks@gmail.com>
2018-09-01 00:16:41 +0530

    aSHAGJS

--- commanmodule.py
+++ commanmodule.py
@@ -10,11 +10,10 @@ webbrowser.open_new_tab('https://www.youtube.com/=%s'%youtube)
 
 #ques3
 import os
-images_path = "D:\python\assignment\comman module"
-image_list = os.listdir('.')
-for i,  image in enumerate(image_list):
-    ext = os.path.splitext(image)[1]
-    if ext == '.jpg':
-        src = images_path + image
-        dst = images_path + str(i) + '.jpg'
+path="D:\python\assignment\comman module"
+filename=listdir('.')
+for name in filename:
+        src = path + image
+        dst = path + str(i) + '.jpg'
         os.rename(src, dst)
+
